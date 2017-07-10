9f9be24031ab9e433c29dfb786a413f4000443df
Benjamin Perez <benjamin.perez@web-education.net>
2017-07-10 15:47:36 +0200

    Fix typos in migration script

--- deployment/migration/0.6.0/includeCollectiviteIds.js
+++ deployment/migration/0.6.0/includeCollectiviteIds.js
@@ -2,11 +2,13 @@ var plateforme = db.Xiti.findOne({"ID_COLLECTIVITE":{$exists:true}});
 if(plateforme != null){
     var structureMaptmp = plateforme.structureMap;
     var setMap = {};
+	var id;
     for(s in structureMaptmp){
+		id  = structureMaptmp[s];
         setMap[s] =
             {
-                "id" : NumberInt(structureMaptmp[s]),
-                "collectiveId" : NumberInt(plateforme.ID_COLLECTIVITE)
+                "id" : NumberInt(id),
+                "collectiviteId" : NumberInt(plateforme.ID_COLLECTIVITE)
             }
     }
     db.Xiti.update({}, {"$set": {"structureMap" : setMap}})
